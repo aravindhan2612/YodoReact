e6b37a27c3fd5f800ea5b2f28e605e9a109071b1
aravindhan2612 <aravindhan2612@gmail.com>
2021-11-13 12:27:54 +0530

    init

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView,Image } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1}>Hello Aravind entering longer text to see propeties if the text view in react native Hello Aravind entering longer text to see propeties if the text view in react native </Text>
+      <Image 
+      blurRadius={0}
+      fadeDuration={5000}
+      source={{
+        width:200,height:200,
+        uri:"https://picsum.photos/200"}}/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#33FFD5',
+    justifyContent:"center",
+    alignItems:"center"
   },
 });
+const STYLES = ['default', 'dark-content', 'light-content'];
+const TRANSITIONS = ['fade', 'slide', 'none'];
